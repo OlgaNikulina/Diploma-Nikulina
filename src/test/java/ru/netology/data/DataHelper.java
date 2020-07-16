fd46ec0a463ce99e73eac1624d4c65a31871478d
OlgaNikulina <nikulinaov@inbox.ru>
2020-07-17 00:50:45 +0500

    SQL-request added

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -1,6 +1,10 @@
 package ru.netology.data;
 
 import lombok.Value;
+import lombok.val;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DataHelper {
     private DataHelper() {
@@ -15,9 +19,21 @@ public class DataHelper {
         private String cvv;
     }
 
-    public static CardsInfo getCardsInfoWith41() {
-        return new CardsInfo("4444 4444 4444 4441", "08", "22", "Name", "999");
-    }
+    public static CardsInfo getCardsInfoWith41() throws SQLException {
+        val cardNumberSQL = "SELECT number FROM data.js";
+        try (
+            val conn = DriverManager.getConnection("jdbc:mysql://192.168.99.100:3306/app", "app", "pass");
+            val cardNumberStmt = conn.prepareStatement(cardNumberSQL);
+        ){
+            try (val rs = cardNumberStmt.executeQuery(cardNumberSQL)) {
+                String number = "";
+                while (rs.next()) {
+                    number = rs.getString("number");
+                }
+                return new CardsInfo("4444 4444 4444 4441", "08", "22", "Name", "999");
+            }
+            }
+        };
 
     public static CardsInfo getCardsInfoWith42() {
         return new CardsInfo("4444 4444 4444 4442", "08", "22", "Name", "999");
